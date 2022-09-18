24dcfbbc16ecc437f2f1a138d023ea38d7229628
lvo23 <61534797+lvo23@users.noreply.github.com>
2022-09-18 22:19:47 +0500

    Update ExampleTest.java

--- src/test/java/guru/qa/ExampleTest.java
+++ src/test/java/guru/qa/ExampleTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 public class ExampleTest {
 
     @Test
-    void exampleTestTrue() {
+    void exampleTestFalse() {
 
         Assertions.assertFalse(2 <= 1);
     }
